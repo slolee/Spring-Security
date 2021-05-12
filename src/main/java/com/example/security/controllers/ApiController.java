eb4afd0484cad6b8b73b5b7ab03635a2991f4861
ch4njun <slolee@naver.com>
2021-05-12 23:30:01 +0900

    [project] Init Setting

---
+++ src/main/java/com/example/security/controllers/ApiController.java
@@ -0,0 +1,14 @@
+package com.example.security.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+public class ApiController {
+    @GetMapping
+    public String hello() {
+        return "hello";
+    }
+}
