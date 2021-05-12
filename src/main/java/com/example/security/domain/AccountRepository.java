71d9bb0232efa9741a8e9f6805b6a8aa16e467dd
ch4njun <slolee@naver.com>
2021-05-13 00:15:08 +0900

    [data-modeling]Create Account Repository

--- src/main/java/com/example/security/domain/AccountRepository.java
+++ src/main/java/com/example/security/domain/AccountRepository.java
@@ -1,4 +1,11 @@
 package com.example.security.domain;
 
-public class AccountRepository {
+import org.springframework.data.repository.CrudRepository;
+
+import java.util.Optional;
+import java.util.OptionalInt;
+
+public interface AccountRepository extends CrudRepository<Account, Long> {
+    Optional<Account> findByUserId(String userId);
+    Optional<Account> findBySocialID(long socialId);
 }
